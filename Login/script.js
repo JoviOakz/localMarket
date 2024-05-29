c2de161870bdc30b54549b654b501bd679a9c9b8
João Carvalho <joao.mateussdc@gmail.com>
2024-05-29 11:09:13 -0300

    feat: att login && att loja

--- Login/script.js
+++ Login/script.js
@@ -1,3 +1,16 @@
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 function login() {
   var nome = $("#nome").val();
   var senha = $("#senha").val();
@@ -13,6 +26,20 @@ function login() {
 
     window.location.href = "../Loja/loja.html";
   } else {
-    //logica para se o nome e senha forem incorretos
+    if (nome && senha && nome != "admin" && senha === "admin") {
+      modal.style.display = "block";
+    } else if (nome && senha && nome === "admin" && senha != "admin") {
+      modal.style.display = "block";
+    } else if (nome && senha && nome != "admin" && senha != "admin") {
+      modal.style.display = "block";
+    }
+  }
+}
+
+function showPassword() {
+  if (document.getElementById("cbx2").checked == true) {
+    document.getElementById("senha").type = 'text';
+  } else {
+    document.getElementById("senha").type = 'password'
   }
 }
